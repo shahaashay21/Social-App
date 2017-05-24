e65695dddfb3ff994f76b39bc0055ecaadc9ab85
Aashay Shah <aashay.shah@sjsu.edu>
2017-05-24 09:32:59 -0700

    Conflicts resolved.

--- SocialApp/app/src/main/java/com/project/sjsu/socialapp/Message.java
+++ SocialApp/app/src/main/java/com/project/sjsu/socialapp/Message.java
@@ -1,28 +1,26 @@
-<<<<<<< HEAD
-package com.project.sjsu.socialapp;
+//package com.project.sjsu.socialapp;
+//
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+///**
+// * Created by Hoang on 5/4/2017.
+// */
+//
+//public class Message  extends Fragment {
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View rootView = inflater.inflate(R.layout.message, container, false);
+//
+//        return rootView;
+//    }
+//}
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-/**
- * Created by Hoang on 5/4/2017.
- */
-
-public class Message  extends Fragment {
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.message, container, false);
-
-        return rootView;
-    }
-}
-
-=======
 package com.project.sjsu.socialapp;
 
 import android.support.v7.app.AppCompatActivity;
@@ -36,4 +34,3 @@ public class Message extends AppCompatActivity {
         setContentView(R.layout.activity_message);
     }
 }
->>>>>>> 186a7808ccf4cedf1be50fc06d94fa038e4354da
