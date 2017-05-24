38e5a2eedf9fd895351c8f58881e09fdccb103f7
hoang1127 <hoang1127@gmail.com>
2017-05-23 20:18:17 -0700

    Set Message fragment

---
+++ SocialApp/app/src/main/java/com/project/sjsu/socialapp/Message.java
@@ -0,0 +1,23 @@
+package com.project.sjsu.socialapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Hoang on 5/4/2017.
+ */
+
+public class Message  extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.message, container, false);
+
+        return rootView;
+    }
+}
+
